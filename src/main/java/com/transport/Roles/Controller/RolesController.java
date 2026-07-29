88e6b83c83f18c029e3efce781d166bf06607983
Vinay Kumar Vuppula <vinaykumar.v@tema-systems.com>
2026-07-29 18:22:15 +0530

    chages done on endpoint

--- src/main/java/com/transport/Roles/Controller/RolesController.java
+++ src/main/java/com/transport/Roles/Controller/RolesController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/roles")
+@RequestMapping("/api/roles")
 public class RolesController {
 
     private final RoleService roleService;
